10b488cc4a5d303ce6724365461a88a4fd68546f
Adrien Cullier <adrien.cullier@gmail.com>
2025-08-05 21:41:32 +0200

    feat: improve app icon and zoom level

--- StationFinder/Sources/StationFinderPresentation/NearestStationMap/Model/ZoomLevel.swift
+++ StationFinder/Sources/StationFinderPresentation/NearestStationMap/Model/ZoomLevel.swift
@@ -3,7 +3,7 @@ enum ZoomLevel {
     case medium
     case maximum
     
-    private static let dotThreshold: Double = 5000
+    private static let dotThreshold: Double = 7000
     private static let minimalThreshold: Double = 2000
     
     init(_ zoom: Double) {
